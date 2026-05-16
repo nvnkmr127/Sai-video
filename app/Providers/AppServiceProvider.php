f2e4b7d6d020afda6bc78cdd0bc1e5d85c3c3534
Naveen <nvnkmr127@gmail.com>
2026-05-16 20:33:38 +0530

    feat: force HTTPS scheme in production environment within AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         try {
             if (\Schema::hasTable('settings')) {
                 $siteSettings = [
